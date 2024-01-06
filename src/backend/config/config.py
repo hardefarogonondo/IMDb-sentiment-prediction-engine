18c137c6da3c903ba873ef6765b72f4a934c3cbc
Hardefa Rogonondo <hardefarogonondo@gmail.com>
2024-01-06 21:16:37 +0700

    FastAPI backend updated

---
+++ src/backend/config/config.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class Review(BaseModel):
+    review: str
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,35 +1,28 @@
+# Import Libraries
+from config.config import Review
 from fastapi import FastAPI, HTTPException
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import pickle
 
-app = FastAPI(title="IMDb Sentiment Analysis Engine API")
-
-# Load the model and tokenizer
+# Initialization
 model = load_model('../../models/sentiment_model')
-with open('../../models/tokenizer.pkl', 'rb') as handle:
-    tokenizer = pickle.load(handle)
+with open('../../models/tokenizer.pkl', 'rb') as file:
+    tokenizer = pickle.load(file)
+app = FastAPI()
+max_length = 300
 
-max_length = 300  # Ensure this is the same as during training
 
 @app.post('/predict')
-def predict_sentiment(review: str):
+def predict_sentiment(review: Review):
     try:
-        # Tokenize and pad the input text
-        sequence = tokenizer.texts_to_sequences([review])
+        sequence = tokenizer.texts_to_sequences([review.review])
         padded_sequence = pad_sequences(sequence, maxlen=max_length, padding='post')
-        
-        # Perform the prediction
         prediction = model.predict(padded_sequence)[0][0]
-        
-        # Interpret the prediction probability
         result = "Positive" if prediction > 0.5 else "Negative"
-        confidence = prediction if result == "Positive" else 1 - prediction
-        
+        confidence = float(prediction) if result == "Positive" else float(1 - prediction)
         return {"result": result, "confidence": confidence}
     except Exception as e:
+        print(f"Error: {e}")
+        print(f"Input review: {review.review}")
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
